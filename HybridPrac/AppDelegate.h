aa33513d2030fa723f771539d598c392f8df29de
cpromise <cpromise1@naver.com>
2015-12-01 00:20:55 +0900

    Initial Commit

---
+++ HybridPrac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HybridPrac
+//
+//  Created by SH on 2015. 12. 1..
+//  Copyright © 2015년 SH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HybridPrac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HybridPrac
+//
+//  Created by SH on 2015. 12. 1..
+//  Copyright © 2015년 SH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
